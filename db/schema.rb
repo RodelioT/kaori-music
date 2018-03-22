ff640ced847b740ad2a93a1be97e8160f7f10a3f
Rodelio (RJ) Tagle <rodelio.tagle@hotmail.com>
2018-03-22 14:52:46 -0500

    Created foreign key association

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180322184402) do
+ActiveRecord::Schema.define(version: 20180322193241) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -56,6 +56,8 @@ ActiveRecord::Schema.define(version: 20180322184402) do
     t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "category_id"
+    t.index ["category_id"], name: "index_products_on_category_id"
   end
 
 end
