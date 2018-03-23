7bfa282dc56bae8689355cfceda791379da4744a
Rodelio (RJ) Tagle <rodelio.tagle@hotmail.com>
2018-03-23 13:37:26 -0500

    Grid Used on Products#index
    Not final, but using col-size-6 so all elements within the object fits

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,6 @@
 class ProductsController < ApplicationController
   def index
-    @products = Product.order(created_at: :desc).page(params[:page]).per(10)
+    @products = Product.order(created_at: :desc).page(params[:page]).per(8)
     @categories = Category.all
   end
 
