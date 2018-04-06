3e6994b4d3690c5a70587140a75d1437c6f08020
Rodelio (RJ) Tagle <rodelio.tagle@hotmail.com>
2018-04-06 11:57:11 -0500

    Started creation of /cart/checkoutpage

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,18 +1,16 @@
 class CartController < ApplicationController
   before_action :initialize_session
   before_action :load_shopping_cart
+  before_action :load_subtotal
 
   def show
     @categories = Category.all
 
-    @subtotal = 0
-    @shopping_cart_items.each do |product|
-      @subtotal += product.price
-    end
+
   end
 
   def checkout
-
+    @categories = Category.all
   end
 
   private
@@ -23,5 +21,12 @@ class CartController < ApplicationController
 
   def load_shopping_cart
     @shopping_cart_items = Product.find(session[:shopping_cart])
+  end
+
+  def load_subtotal
+    @subtotal = 0
+    @shopping_cart_items.each do |product|
+      @subtotal += product.price
+    end
   end
 end
